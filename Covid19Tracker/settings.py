7f049e9fed68230ea41d8e48be0cdace0fbd9f11
nimeshm05 <nimeshmysore05@gmail.com>
2021-04-15 15:40:05 +0530

    edit

--- Covid19Tracker/settings.py
+++ Covid19Tracker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hl$1itcg@fvss2umbf(4fq^%*@av)kg0f^ch$jc+m%q8=)%pse'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
